d4a0fc85d1349f15a76699a52a961732fa08daac
mn-napoleon <napoleonyeboahkofi@gmail.com>
2026-03-08 02:30:29 +0100

    Add files via upload
    Add project files for Energy Saver App

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const cacheName = "energy-saver-cache-v1";
+
+const filesToCache = [
+  "/",
+  "index.html",
+  "style.css",
+  "script.js",
+  "energy-logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});a
