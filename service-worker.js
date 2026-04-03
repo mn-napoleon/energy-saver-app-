6849414af4b6db0da2f36ebde555d2fcc93ed702
mn-napoleon <napoleonyeboahkofi@gmail.com>
2026-04-03 21:07:01 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const cacheName = "energy-saver-cache-v1";
+const CACHE_NAME = "energy-saver-v2"; //  change version when you update app
 
-const filesToCache = [
+const urlsToCache = [
   "/",
   "index.html",
   "style.css",
@@ -8,20 +8,37 @@ const filesToCache = [
   "energy-logo.png"
 ];
 
+// INSTALL
 self.addEventListener("install", event => {
+  self.skipWaiting(); //  force new version immediately
+
   event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => {
-        return cache.addAll(filesToCache);
-      })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); //  delete old cache
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim(); //  take control immediately
+});
+
+// FETCH
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
-});a
+});
